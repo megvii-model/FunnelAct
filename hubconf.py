d9dc03caacbbf10de4e39e1c6c33967e7c297b09
nmaac <ma_ning_ning@163.com>
2020-08-04 11:20:35 +0800

    Add hubconf.py

---
+++ hubconf.py
@@ -0,0 +1,14 @@
+from resnet.resnet import (
+    BasicBlock,
+    Bottleneck,
+    Bottleneck_FReLU,
+    ResNet,
+    resnet50_frelu,
+)
+
+from shufflenetv2.shufflenet_v2 import (
+    shufflenet_v2_x0_5,
+    shufflenet_v2_x1_0,
+    shufflenet_v2_x1_5,
+    shufflenet_v2_x2_0,
+)
