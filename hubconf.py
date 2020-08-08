2fcf71d3d2b403acdd214a6b36cea91ecbaac73f
nmaac <ma_ning_ning@163.com>
2020-08-08 11:10:14 +0800

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -2,14 +2,10 @@ import sys
 sys.path.insert(0, "resnet")
 sys.path.insert(0, "shufflenetv2")
 
-from resnet.resnet import (
-    BasicBlock,
-    Bottleneck,
-    Bottleneck_FReLU,
-    ResNet,
+from resnet import (
     resnet50_frelu,
 )
 
-from shufflenetv2.shufflenet_v2 import (
+from shufflenet_v2 import (
     shufflenet_v2_x0_5,
 )
